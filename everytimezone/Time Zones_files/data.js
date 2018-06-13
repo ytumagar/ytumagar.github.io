c7bae20934adcfb1c7b72cda1766fc73b140479d
ytumagar <ytumagar@yahoo.co.jp>
2018-06-13 15:02:18 +0900

    Add files via upload

---
+++ everytimezone/Time Zones_files/data.js
@@ -0,0 +1,44 @@
+var data = [
+  [
+    -10,
+    "Honolulu",
+    "HST UTC-10",
+    true
+  ],
+  [
+    -7,
+    "San Francisco",
+    "PDT UTC-7",
+    true
+  ],
+  [
+    -5,
+    "Chicago",
+    "CDT UTC-5",
+    true
+  ],
+  [
+    -4,
+    "New York",
+    "EDT UTC-4",
+    true
+  ],
+  [
+    0,
+    "UTC",
+    "",
+    true
+  ],
+ [
+    9,
+    "Tokyo",
+    "JST UTC+9",
+    true
+  ],
+  [
+    10,
+    "Sydney",
+    "AEST UTC+10",
+    true
+  ]
+];

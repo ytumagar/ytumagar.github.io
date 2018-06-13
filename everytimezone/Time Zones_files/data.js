e6985486351cdab403175ca951e939aae143fe2f
ytumagar <ytumagar@yahoo.co.jp>
2018-06-13 15:11:21 +0900

    Update data.js

--- everytimezone/Time Zones_files/data.js
+++ everytimezone/Time Zones_files/data.js
@@ -10,35 +10,5 @@ var data = [
     "San Francisco",
     "PDT UTC-7",
     true
-  ],
-  [
-    -5,
-    "Chicago",
-    "CDT UTC-5",
-    true
-  ],
-  [
-    -4,
-    "New York",
-    "EDT UTC-4",
-    true
-  ],
-  [
-    0,
-    "UTC",
-    "",
-    true
-  ],
- [
-    9,
-    "Tokyo",
-    "JST UTC+9",
-    true
-  ],
-  [
-    10,
-    "Sydney",
-    "AEST UTC+10",
-    true
   ]
 ];
